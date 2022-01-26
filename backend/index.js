f7b2cc189cc538ae0a5aacf9a537384b2792a8fa
Adam Żbikowski <ajzbikowski@gmail.com>
2022-01-26 13:56:54 +0100

    Setup MySQL database connection

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,23 @@ const fs = require("fs");
 require("dotenv").config();
 const express = require("express");
 var mysql = require("mysql");
+
+// Setup connection to MySQL database
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database", err);
+  }
+
+  console.log("Database connected");
+});
+
 const app = express();
 const port = 8081;
 
