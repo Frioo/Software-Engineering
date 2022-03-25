88763f3d5ac78da1443450fecf0e49abf089c607
Max Ford <74331798+Max-ford@users.noreply.github.com>
2022-03-25 18:55:09 +0000

    Adding GetNCountries test

--- src/db.test.js
+++ src/db.test.js
@@ -2,17 +2,30 @@ import Prisma from "@prisma/client";
 import { getCountries } from "./db.mjs";
 const { PrismaClient } = Prisma;
 
-async function getCountriesAsia() {
+async function getContinentCountries(continent) {
   const options = {
-    continent: "Asia"
-  };
+    continent
+  }
   const res = await getCountries(options);
   return res;
 }
 
+async function getNCountries(limit) {
+  const options = {
+    limit
+  }
+  const res = await getCountries(options)
+  return res.length
+}
 test('GetCountries continent filter', async () => {
-  const countries = await getCountriesAsia();
+  const countries = await getContinentCountries('Asia');
   countries.forEach(c => {
     expect(c.Continent).toBe("Asia");
   })
 })
+
+test('GetNCountries', async () => {
+  let limit = 10
+  const res = await getNCountries(limit)
+  expect(res).toBe(limit)
+}) 
