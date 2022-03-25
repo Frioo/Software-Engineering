a2166c367fca37c7742ed3af1331bf8dc41ef64f
Max Ford <74331798+Max-ford@users.noreply.github.com>
2022-03-25 18:59:59 +0000

    Clean up code & added region testing

--- src/db.test.js
+++ src/db.test.js
@@ -17,10 +17,19 @@ async function getNCountries(limit) {
   const res = await getCountries(options)
   return res.length
 }
+
+async function getRegionCountries(region) {
+  const options = {
+    region
+  }
+  const res = await getCountries(options)
+  return res;
+}
 test('GetCountries continent filter', async () => {
-  const countries = await getContinentCountries('Asia');
+  let continent = 'Asia'
+  const countries = await getContinentCountries(continent);
   countries.forEach(c => {
-    expect(c.Continent).toBe("Asia");
+    expect(c.Continent).toBe(continent);
   })
 })
 
@@ -29,3 +38,12 @@ test('GetNCountries', async () => {
   const res = await getNCountries(limit)
   expect(res).toBe(limit)
 }) 
+
+test('GetCountries region filter', async () => {
+  let region = 'Middle East'
+  const countries = await getRegionCountries(region)
+  countries.forEach(element => {
+    expect(element.Region).toBe(region)
+  })
+
+})
